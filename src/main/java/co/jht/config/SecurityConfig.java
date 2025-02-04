84cfebdd595dd87c80f4c2bf9de228a99bb40be6
Jonathan <35850491+J-H-Tran@users.noreply.github.com>
2025-02-04 13:34:13 +0900

    Feature/user authentication (#1)
    * able to authenticate registered user
    
    * include favicon resource

--- src/main/java/co/jht/config/SecurityConfig.java
+++ src/main/java/co/jht/config/SecurityConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +13,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/home", "/register", "/login").permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers(
+                        "/", "/home", "/register", "/login",
+                        "/css/**", "/js/**", "/images/**", "/static/favicon.ico"
+                ).permitAll()
+                .anyRequest()
+                .authenticated()
             )
             .formLogin(form -> form
-                .loginPage("/login")
+                .loginPage("/loginSuccess")
                 .defaultSuccessUrl("/home", true) // Redirect to /home after successful login
                 .permitAll()
             )
--- src/main/java/co/jht/service/impl/UserServiceImpl.java
+++ src/main/java/co/jht/service/impl/UserServiceImpl.java
@@ -13,6 +13,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override

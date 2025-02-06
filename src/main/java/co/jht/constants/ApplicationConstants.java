b53205a6dd96c0adace0dd7fe0353fc668e6b401
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-07 02:02:54 +0900

    added date time format const for tokyo

--- src/main/java/co/jht/constants/ApplicationConstants.java
+++ src/main/java/co/jht/constants/ApplicationConstants.java
@@ -2,4 +2,5 @@ package co.jht.constants;
 
 public class ApplicationConstants {
     public static final String ASIA_TOKYO = "Asia/Tokyo";
+    public static final String ASIA_TOKYO_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX";
 }

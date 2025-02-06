01041a9900c91b515916c3b7cf1df98b7230589f
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-07 02:04:27 +0900

    update dto with json property

--- src/main/java/co/jht/model/domain/response/dto/appuser/AppUserDTO.java
+++ src/main/java/co/jht/model/domain/response/dto/appuser/AppUserDTO.java
@@ -2,34 +2,55 @@ package co.jht.model.domain.response.dto.appuser;
 
 import co.jht.enums.UserRole;
 import co.jht.enums.UserStatus;
+import co.jht.serializer.ZonedDateTimeSerializer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.time.ZonedDateTime;
 
 public class AppUserDTO {
-    private Long appUserId;
-    private String userName;
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
+    @JsonProperty("profile_picture_url")
     private String profilePictureUrl;
+
+    @JsonProperty("registration_date")
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
     private ZonedDateTime registrationDate;
+
+    @JsonProperty("account_status")
     private UserStatus accountStatus;
+
+    @JsonProperty("role")
     private UserRole role;
 
-    public Long getAppUserId() {
-        return appUserId;
+    public String getUsername() {
+        return username;
     }
 
-    public void setAppUserId(Long appUserId) {
-        this.appUserId = appUserId;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getPassword() {
+        return password;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {

80218763b7aeb60f1727dee44e461fb08df283f5
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-07 02:08:20 +0900

    update type typo

--- src/main/java/co/jht/service/UserService.java
+++ src/main/java/co/jht/service/UserService.java
@@ -1,7 +1,6 @@
 package co.jht.service;
 
 import co.jht.model.domain.persist.entity.appuser.AppUser;
-import co.jht.model.domain.response.dto.appuser.AppUserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
@@ -9,7 +8,7 @@ import java.util.List;
 public interface UserService extends UserDetailsService {
     List<AppUser> getAllUsers();
     AppUser getUserById(Long userId);
-    AppUser createUser(AppUserDTO userDTO);
+    AppUser createUser(AppUser user);
     AppUser updateUser(AppUser user);
     void deleteUser(Long userId);
     void registerUser(String username, String password, String email);

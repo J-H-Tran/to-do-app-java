b5d382b73ba17c58d6c0054ec5cf146d27ecdae8
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-06 22:05:16 +0900

    update entity classes

---
+++ src/main/java/co/jht/model/domain/persist/entity/tasks/TaskDueDate.java
@@ -0,0 +1,22 @@
+package co.jht.model.domain.persist.entity.tasks;
+
+import co.jht.serializer.ZonedDateTimeDeserializer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.time.ZonedDateTime;
+
+public class TaskDueDate {
+
+    @JsonProperty("due_date")
+    @JsonDeserialize(using = ZonedDateTimeDeserializer.class)
+    private ZonedDateTime dueDate;
+
+    public ZonedDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(ZonedDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+}

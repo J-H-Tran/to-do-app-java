242080044721c8e29b7c341500caee7e79ab18c0
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-07 02:04:27 +0900

    update dto with json property

--- src/main/java/co/jht/model/domain/response/dto/tasks/TaskItemDTO.java
+++ src/main/java/co/jht/model/domain/response/dto/tasks/TaskItemDTO.java
@@ -1,14 +1,32 @@
 package co.jht.model.domain.response.dto.tasks;
 
+import co.jht.serializer.ZonedDateTimeSerializer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.time.ZonedDateTime;
 
 public class TaskItemDTO {
     private Long taskId;
+
+    @JsonProperty("title")
     private String title;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("creation_date")
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
     private ZonedDateTime creationDate;
+
+    @JsonProperty("due_date")
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
     private ZonedDateTime dueDate;
+
+    @JsonProperty("complete_status")
     private boolean completeStatus;
+
+    @JsonProperty("user_id")
     private Long userId;
 
     public Long getTaskId() {

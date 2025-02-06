3dd460ea612269518dae143cddacdfbdd67f4f37
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-07 02:08:58 +0900

    added method for current tokyo zoned date time

--- src/main/java/co/jht/util/DateTimeFormatterUtil.java
+++ src/main/java/co/jht/util/DateTimeFormatterUtil.java
@@ -1,11 +1,20 @@
 package co.jht.util;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static co.jht.constants.ApplicationConstants.ASIA_TOKYO;
+import static co.jht.constants.ApplicationConstants.ASIA_TOKYO_FORMAT;
+
 public class DateTimeFormatterUtil {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(ASIA_TOKYO_FORMAT);
 
     public static DateTimeFormatter getFormatter() {
         return FORMATTER;
+    }
+
+    public static ZonedDateTime getCurrentTokyoTime() {
+        return ZonedDateTime.parse(ZonedDateTime.now(ZoneId.of(ASIA_TOKYO)).format(DateTimeFormatterUtil.getFormatter()));
     }
 }

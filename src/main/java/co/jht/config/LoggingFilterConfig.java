bc985358f7dc07a436e6abf889e7ba4ae0df1f6e
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-10 03:08:25 +0900

    rename for clarity

--- src/main/java/co/jht/config/LoggingFilterConfig.java
+++ src/main/java/co/jht/config/LoggingFilterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FilterConfig {
+public class LoggingFilterConfig {
 
     @Bean
     public FilterRegistrationBean<LoggingFilter> loggingFilter() {

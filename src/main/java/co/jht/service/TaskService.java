66bf39238c293ac41d3a0ec566a200330a9eec2b
J-H-Tran <tr.h.jonathan@gmail.com>
2025-02-11 02:08:31 +0900

    comment out unneeded files

--- src/main/java/co/jht/service/TaskService.java
+++ src/main/java/co/jht/service/TaskService.java
@@ -1,15 +1,15 @@
-package co.jht.service;
-
-import co.jht.model.domain.entity.tasks.TaskItem;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-
-public interface TaskService {
-    List<TaskItem> getTasksByUserId(Long userId);
-    TaskItem createTask(TaskItem task);
-    TaskItem updateTask(TaskItem task);
-    void deleteTask(TaskItem task);
-    TaskItem updateTaskDueDate(Long taskId, ZonedDateTime dueDate);
-    TaskItem updateTaskCompleteStatus(Long taskId, boolean completeStatus);
-}
+//package co.jht.service;
+//
+//import co.jht.model.domain.entity.tasks.TaskItem;
+//
+//import java.time.ZonedDateTime;
+//import java.util.List;
+//
+//public interface TaskService {
+//    List<TaskItem> getTasksByUserId(Long userId);
+//    TaskItem createTask(TaskItem task);
+//    TaskItem updateTask(TaskItem task);
+//    void deleteTask(TaskItem task);
+//    TaskItem updateTaskDueDate(Long taskId, ZonedDateTime dueDate);
+//    TaskItem updateTaskCompleteStatus(Long taskId, boolean completeStatus);
+//}
